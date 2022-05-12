6a8a278bf7c093770ec5da826b168936aff56586
paulwrath1223 <paul@fornage.net>
2022-05-12 11:40:14 -0700

    init

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-with open('readme.txt') as f:
+with open('Whole mogus external perim only.txt') as f:
     lines = f.readlines()
+
+
+relevant_lines_list = []
+relevant_line = False
+for line in lines:
+    filtered_line = ""
+    if line == "G1 F8640":
+        relevant_line = False
+    elif line == "G1 F900":
+        relevant_line = True
+    if relevant_line:
+
+
+
+        relevant_lines_list.append(line)
+
+
